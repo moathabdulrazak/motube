39511357960c8894151fb6e6e27c618434dd52e7
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-27 02:49:22 -0600

    ss

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,50 +1,120 @@
-import React from 'react'
-import styled from "styled-components"
-import HomeIcon from '@mui/icons-material/Home';
+import React from "react";
+import styled from "styled-components";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
 const Container = styled.div`
-  flex:1;
+  flex: 1;
   background-color: #202020;
   height: 100vh;
-  color:white;
+  color: white;
 `;
 const Wrapper = styled.div`
-padding: 18px 26px;
-
+  padding: 18px 26px;
 `;
 const Logo = styled.div`
-display: flex;
-align-items:center;
-gap: 5px;
-font-weight: bold;
-margin-bottom: 25px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
 `;
 const Img = styled.img`
   height: 50px;
-
-`
+`;
 
 const Item = styled.div`
-display: flex;
-align-items:center;
-gap: 20px;
-cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 7.5px 0px ;
+  gap: 20px;
+  cursor: pointer;
 `;
 
 const Menu = () => {
   return (
     <Container>
       <Wrapper>
-       <Logo>
-        <Img src="https://cdn.dribbble.com/userupload/2445208/file/original-36e8f98866a8475c749554b2e3cecf95.png?resize=400x0" />
-        MoTube
-       </Logo>
-       <Item>
-        <HomeIcon/>
-        Home
-       </Item>
+        <Logo>
+          <Img src="https://cdn.dribbble.com/userupload/2445208/file/original-36e8f98866a8475c749554b2e3cecf95.png?resize=400x0" />
+          MoTube
+        </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon />
+          Subscriptions
+        </Item>
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlinedIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
+        <Item>
+          <SettingsBrightnessOutlinedIcon />
+          Light Mode
+        </Item>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
