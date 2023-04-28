c927f61bb46b2d03fd87addcdcd0f92ebaf36391
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-28 04:30:44 -0600

    Setting up for tmmrow. Going to sleep.

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -3,6 +3,19 @@ import {} from '../controllers/users.controller.js'
 
 const router = express.Router()
 
+// get a user
+
+// update a user
+
+// delete a user
+
+// subscribe to a user
+
+// unsubscribe to a user
+
+// like a video
+
+// dislike a video
 
 
 
