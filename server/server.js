8ace8890f21f7526fa4686449b038a089a516712
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-28 03:04:40 -0600

    Mongo connected!

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 import express from "express";
-
-
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
 
 const app = express()
+dotenv.config()
+mongoose.set("strictQuery", true);
 
+const connect = async () => {
+
+  try {
+    if (!process.env.MONGO) {
+      console.error("MONGODB not found in environment variables!");
+      process.exit(1);
+    }
+    
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 
 
 app.listen(8800, () => {
-  console.log("Connected");
+  connect()
+  console.log("Backend Server running");
 })
