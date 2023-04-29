a4be412bd35c11a7eec5412d5c23de751511ddd9
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-29 03:11:33 -0600

    s

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,12 +1,13 @@
 import  express from "express";
 import {updateUser, deleteUser, getUser, subscribe, unsubscribe, like, dislike} from '../controllers/users.controller.js'
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router()
 
 // get a user
-router.get('/find/:id', getUser)
+router.get('/find/:id',  getUser)
 // update a user
-router.put('/:id', updateUser)
+router.put('/:id', verifyToken, updateUser)
 // delete a user
 router.delete('/:id', deleteUser)
 // subscribe to a user
