200ee913c18ca7eb446f49b8b556ea0849f772eb
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-29 03:06:31 -0600

    s

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -4,19 +4,19 @@ import {updateUser, deleteUser, getUser, subscribe, unsubscribe, like, dislike} 
 const router = express.Router()
 
 // get a user
-
+router.get('/find/:id', getUser)
 // update a user
 router.put('/:id', updateUser)
 // delete a user
-
+router.delete('/:id', deleteUser)
 // subscribe to a user
-
+router.put('/sub/:id', subscribe)
 // unsubscribe to a user
-
+router.put('/unsub/:id', unsubscribe)
 // like a video
-
+router.put('/like/:videoId', like)
 // dislike a video
-
+router.put('/dislike/:videoId', dislike)
 
 
 export default router
