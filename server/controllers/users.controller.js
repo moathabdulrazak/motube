d9ee51afd618b80a8c6b91d9544dfb80395fa972
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-29 03:03:42 -0600

    setting up.

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,5 +1,23 @@
 
 
-export const fn = async  (req, res) => {
+export const updateUser = async  (req, res, next) => {
+
+}
+export const deleteUser = async  (req, res) => {
+
+}
+export const getUser = async  (req, res) => {
+
+}
+export const subscribe = async  (req, res) => {
+
+}
+export const unsubscribe = async  (req, res) => {
+
+}
+export const like = async  (req, res) => {
+
+}
+export const dislike = async  (req, res) => {
 
 }
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,12 +1,12 @@
 import  express from "express";
-import {} from '../controllers/users.controller.js'
+import {updateUser, deleteUser, getUser, subscribe, unsubscribe, like, dislike} from '../controllers/users.controller.js'
 
 const router = express.Router()
 
 // get a user
 
 // update a user
-
+router.put('/:id', updateUser)
 // delete a user
 
 // subscribe to a user
