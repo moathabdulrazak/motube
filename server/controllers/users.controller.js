ae42e4640cdbe4739a97ec9622f631086c4856d7
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-29 03:37:58 -0600

    get User BE

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -33,7 +33,15 @@ export const deleteUser = async (req, res, next) => {
     return next(createError(403, "You can delete only your account!"));
   }
 };
-export const getUser = async (req, res) => {};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const subscribe = async (req, res) => {};
 export const unsubscribe = async (req, res) => {};
 export const like = async (req, res) => {};
