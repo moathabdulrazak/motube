dceb69caec0debe579d3890fafe54fc11924ff79
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-30 02:23:43 -0600

    ss

--- server/routes/videos.route.js
+++ server/routes/videos.route.js
@@ -13,4 +13,16 @@ router.put("/:id", verifyToken, updateVideo)
 router.delete("/:id", verifyToken, deleteVideo)
 // get video
 router.get("/find/:id", getVideo)
+// add views
+router.put("/view/:id", addView)
+// get trending videos
+router.get("/trend", trend)
+// get random videos
+router.get("/random", random)
+// subscribe
+router.get("/sub",verifyToken, sub)
+// get tags
+router.get("/tags", getByTag)
+// search
+router.get("/search", search)
 export default router
