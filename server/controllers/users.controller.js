c1f6b27b98648bf58f6c3e4d74b37d10fd1f2608
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-30 02:11:48 -0600

    subsrcibe BE

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -42,7 +42,20 @@ export const getUser = async (req, res, next) => {
   }
 };
 
-export const subscribe = async (req, res) => {};
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+    res.status(200).json("Subscribed.")
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const unsubscribe = async (req, res) => {};
 export const like = async (req, res) => {};
 export const dislike = async (req, res) => {};
