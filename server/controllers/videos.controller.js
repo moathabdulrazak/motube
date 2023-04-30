eaa52c81f5d76eff482ec6a0ec0ecb51d5b98035
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-04-30 02:19:40 -0600

    s

--- server/controllers/videos.controller.js
+++ server/controllers/videos.controller.js
@@ -1,5 +1,21 @@
 
+import User from "../models/User.js";
+import Video from "../models/Video.js";
+export const addVideo = async (req, res, next) => {
+  const newVideo = new Video({ userId: req.user.id, ...req.body });
+  try {
+    const savedVideo = await newVideo.save();
+    res.status(200).json(savedVideo);
+  } catch (err) {
+    next(err);
+  }
+};
+export const updateVideo = async  (req, res, next) => {
 
-export const fn = async  (req, res) => {
+}
+export const deleteVideo = async  (req, res, next) => {
+
+}
+export const getVideo = async  (req, res, next) => {
 
 }
--- server/routes/videos.route.js
+++ server/routes/videos.route.js
@@ -1,8 +1,16 @@
 import  express from "express";
-import {} from '../controllers/videos.controller.js'
+import {addVideo, deleteVideo, getVideo, updateVideo} from '../controllers/videos.controller.js'
+import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router()
 
 
 
-
+//create a video
+router.post("/", verifyToken, addVideo)
+// update video
+router.put("/:id", verifyToken, updateVideo)
+// delete video
+router.delete("/:id", verifyToken, deleteVideo)
+// get video
+router.get("/find/:id", getVideo)
 export default router
