7a3dcdfb2c7dd7c9e3adcdc743634edefa21860e
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-01 00:56:46 -0600

    if current user dont displaay sign in. in menu tab

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -17,6 +17,7 @@ import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Container = styled.div`
   flex: 1;
   background-color: ${({ theme }) => theme.bgLighter};
@@ -82,6 +83,7 @@ const Title = styled.h2`
 
 
 const Menu = ({darkMode, setDarkMode}) => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Container>
       <Wrapper>
@@ -117,6 +119,7 @@ const Menu = ({darkMode, setDarkMode}) => {
           History
         </Item>
         <Hr/>
+       { !currentUser && <>
         <Login>
           Sign in to like videos, comment and subscribe.
           <Link to={'signin'} style={{textDecoration: 'none'}}  >
@@ -124,6 +127,7 @@ const Menu = ({darkMode, setDarkMode}) => {
           </Link>
         </Login>
         <Hr/>
+        </>}
         <Title>
           Best of Mo tube
         </Title>
