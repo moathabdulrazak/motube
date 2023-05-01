73173f541a9e67c7aa78c416248f1935ff6a6573
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-01 00:54:04 -0600

    current user redux. displaying in navbar.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
+import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
 const Container = styled.div`
   position: sticky;
   top: 0;
@@ -33,6 +34,21 @@ const Search = styled.div`
   border: 1px solid #ccc;
   border-radius: 3px;
 `;
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Avatar = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: #999;
+`;
+
 
 const Input = styled.input`
   border: none;
@@ -54,6 +70,7 @@ const Button = styled.button`
   gap: 5px;
 `;
 const Navbar = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Container>
       <Wrapper>
@@ -61,12 +78,20 @@ const Navbar = () => {
           <Input placeholder="Search" />
           <SearchOutlinedIcon />
         </Search>
-        <Link to={'signin'} style={{textDecoration: 'none'}}  >
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
-        </Link>
+        {currentUser ? (
+          <User>
+            <VideoCallOutlinedIcon/>
+            <Avatar/>
+            {currentUser.name}
+          </User>
+        ) : (
+          <Link to={"signin"} style={{ textDecoration: "none" }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>
+        )}
       </Wrapper>
     </Container>
   );
